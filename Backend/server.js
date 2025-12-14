0a038ac1f8a6bb68b93b9c55b291d5787307b104
vinay348 <vinaykumar.gsl225@gmail.com>
2025-12-14 21:54:05 +0530

    third commit

--- Backend/server.js
+++ Backend/server.js
@@ -30,10 +30,10 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get(/(.*)/, (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
 	});
 }
 
